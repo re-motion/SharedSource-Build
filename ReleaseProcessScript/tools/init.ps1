d4cc87cf5a0aeda88e7171650026be697dbab211
Florian Decker <florian.decker@rubicon.eu>
2016-04-05 08:47:47 +0200

    Fix secondary bug of RMSRCBUILD-49, Message for get-help now only gets displayed once at solution startup

--- ReleaseProcessScript/tools/init.ps1
+++ ReleaseProcessScript/tools/init.ps1
@@ -1,4 +1,9 @@
 param($installPath, $toolsPath, $package)
-Write-Output "Type 'get-help ReleaseProcessScript' to see all available ReleaseProcessScript commands"
+
+if(-not $global:AfterFirstInit)
+{
+  Write-Host "Type 'get-help ReleaseProcessScript' to see all available ReleaseProcessScript commands."
+  $global:AfterFirstInit = $TRUE
+}
 
 Import-Module (Join-Path $toolsPath ReleaseProcessScript.psm1) -DisableNameChecking -Force
