7d783990c06fa6486f1d4ca0cb81540216117e6f
Florian Decker <florian.decker@rubicon.eu>
2016-08-26 13:53:37 +0200

    Update version to '2.0.0-alpha.15'.

--- AssemblyInfoShared.cs
+++ AssemblyInfoShared.cs
@@ -24,5 +24,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyConfiguration ("Build type: Debug")]
 [assembly: AssemblyVersion ("2.0.0.0")]
-[assembly: AssemblyFileVersion ("2.0.0.01011")]
-[assembly: AssemblyInformationalVersion ("2.0.0-alpha.11+Debug")]
+[assembly: AssemblyFileVersion ("2.0.0.01015")]
+[assembly: AssemblyInformationalVersion ("2.0.0-alpha.15+Debug")]
