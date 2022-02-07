4318f7bca9cd80a28ed69a6706c70f82ff3f5a9f
Christian Weissenbacher <c.weissenbacher@rubicon.eu>
2022-02-07 11:55:11 +0100

    RMSRCBUILD-191 compile test projects target

--- BuildScript.Nuke/Build.Compile.cs
+++ BuildScript.Nuke/Build.Compile.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Nuke.Common;
 using Nuke.Common.Tools.MSBuild;
 using Nuke.Common.Utilities.Collections;
@@ -7,37 +6,71 @@ using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 
 internal partial class Build : NukeBuild
 {
-  public Target CompileReleaseBuild => _ => _
+  private Target CompileReleaseBuild => _ => _
+      .DependsOn(ImportDefinitions, RestoreReleaseBuild)
+      .Executes(() =>
+      {
+        ReleaseProjectFiles.ForEach(CompileProject);
+      });
+
+  private Target CompileTestBuild => _ => _
+      .DependsOn(ImportDefinitions, RestoreTestBuild)
+      .Executes(() =>
+      {
+        TestProjectFiles.ForEach(CompileProject);
+      });
+
+  private Target RestoreReleaseBuild => _ => _
       .DependsOn(ImportDefinitions)
       .Executes(() =>
       {
-        ReleaseProjectFiles.ForEach(projectFile =>
-        {
-          var targets = new List<string> { "Build" };
-          if (projectFile.IsMultiTargetFramework)
-            targets.Add("DispatchToInnerBuilds");
-
-          Configuration.ForEach(config =>
-          {
-            MSBuild(s => s
-                .SetTargetPath(projectFile.Path)
-                .SetTargets(targets)
-                .SetConfiguration(config)
-                .SetAssemblyVersion(VersionHelper.AssemblyVersion)
-                .SetFileVersion(VersionHelper.AssemblyFileVersion)
-                .SetInformationalVersion(VersionHelper.GetAssemblyInformationVersion(config, AdditionalBuildMetadata))
-                .SetCopyright(AssemblyMetadata.Copyright)
-                .SetProperty("CompanyName", AssemblyMetadata.CompanyName)
-                .SetProperty("CompanyUrl", AssemblyMetadata.CompanyUrl)
-                .SetProperty("ProductName", AssemblyMetadata.ProductName)
-                .SetProperty("AssemblyOriginatorKeyFile", DirectoryHelper.SolutionKeyFile)
-                .SetProperty("OutputDirectory", DirectoryHelper.OutputDirectory)
-                .SetProperty("TempDirectory", DirectoryHelper.TempDirectory)
-                .SetProperty("LogDirectory", DirectoryHelper.LogDirectory)
-                .SetPackageProjectUrl(GitRepository.HttpsUrl)
-                .SetToolsVersion(projectFile.ToolsVersion)
-            );
-          });
-        });
+        ReleaseProjectFiles.ForEach(RestoreProject);
       });
+
+  private Target RestoreTestBuild => _ => _
+      .DependsOn(ImportDefinitions)
+      .Executes(() =>
+      {
+        TestProjectFiles.ForEach(RestoreProject);
+      });
+
+  private void CompileProject (ProjectMetadata projectFile)
+  {
+    var targets = GetCompileTargets(projectFile);
+    Configuration.ForEach(
+        config =>
+        {
+          MSBuild(s => s
+              .SetTargetPath(projectFile.Path)
+              .SetTargets(targets)
+              .SetConfiguration(config)
+              .SetAssemblyVersion(VersionHelper.AssemblyVersion)
+              .SetFileVersion(VersionHelper.AssemblyFileVersion)
+              .SetInformationalVersion(VersionHelper.GetAssemblyInformationVersion(config, AdditionalBuildMetadata))
+              .SetCopyright(AssemblyMetadata.Copyright)
+              .SetProperty("PackageVersion", VersionHelper.AssemblyNuGetVersion)
+              .SetProperty("CompanyName", AssemblyMetadata.CompanyName)
+              .SetProperty("CompanyUrl", AssemblyMetadata.CompanyUrl)
+              .SetProperty("ProductName", AssemblyMetadata.ProductName)
+              .SetProperty("AssemblyOriginatorKeyFile", DirectoryHelper.SolutionKeyFile)
+              .SetPackageProjectUrl(GitRepository.HttpsUrl)
+              .SetToolsVersion(projectFile.ToolsVersion)
+          );
+        });
+  }
+
+  private void RestoreProject (ProjectMetadata projectFile)
+  {
+    MSBuild(s => s
+        .SetTargetPath(projectFile.Path)
+        .SetTargets("Restore")
+    );
+  }
+
+  private string GetCompileTargets (ProjectMetadata project)
+  {
+    if (project.IsMultiTargetFramework)
+      return "DispatchToInnerBuilds";
+    return "Build";
+  }
 }
