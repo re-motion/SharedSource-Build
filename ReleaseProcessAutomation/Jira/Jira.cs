1b2a383a6092263c9909ea090e7991ede18551a8
Lukas Hartinger <lukas.hartinger@rubicon.eu>
2022-05-09 09:42:53 +0200

    RMSRCBUILD-268 Jira integration without credentials

---
+++ ReleaseProcessAutomation/Jira/Jira.cs
@@ -0,0 +1,105 @@
+using ReleaseProcessAutomation.Configuration.Data;
+using ReleaseProcessAutomation.SemanticVersioning;
+using Serilog;
+using Spectre.Console;
+
+namespace ReleaseProcessAutomation.Jira;
+
+public interface IJira
+{
+  void CreateAndReleaseJiraVersion (SemanticVersion currentVersion, SemanticVersion nextVersion, bool squashUnreleased = false);
+}
+
+public class Jira : IJira
+{
+  private readonly Config _config;
+  private readonly IAnsiConsole _console;
+  private readonly string _jiraUrlPostfix;
+  private readonly ILogger _log = Log.ForContext<Jira>();
+  public Jira (Config config, IAnsiConsole console, string jiraUrlPostfix)
+  {
+    _config = config;
+    _console = console;
+    _jiraUrlPostfix = jiraUrlPostfix;
+  }
+  
+  public void CreateAndReleaseJiraVersion (SemanticVersion currentVersion, SemanticVersion nextVersion, bool squashUnreleased = false)
+  {
+    var currentVersionID = CreateVersion(currentVersion);
+    var nextVersionID = CreateVersion(nextVersion);
+
+    var releaseMessage = $"Releasing version '{currentVersion}' on JIRA. ";
+    _log.Information(releaseMessage);
+    _console.WriteLine(releaseMessage);
+    
+    var moveMessage = $"Moving open issues to '{nextVersion}'.";
+    _log.Information(moveMessage);
+    _console.WriteLine(moveMessage);
+
+    ReleaseVersion(currentVersionID, nextVersionID, squashUnreleased);
+  }
+
+  private string CreateVersion (SemanticVersion version)
+  {
+    var jiraCreateVersion = new JiraCreateNewVersionWithVersionNumber
+                            {
+                                JiraUrl = JiraUrlWithPostfix(),
+                                JiraProjectKey = _config.Jira.JiraProjectKey,
+                                VersionNumber = version.ToString()
+                            };
+    
+    AddAuthentication(jiraCreateVersion);
+    
+    jiraCreateVersion.Execute();
+
+    return jiraCreateVersion.CreatedVersionID;
+  }
+
+  private string JiraUrlWithPostfix ()
+  {
+    var url = _config.Jira.JiraURL;
+    return url.EndsWith("/") ? $"{url}{_jiraUrlPostfix}" : $"{url}/{_jiraUrlPostfix}";
+  }
+
+  private void AddAuthentication (JiraTask task)
+  {
+    if (_config.Jira.UseNTLM)
+      return;
+    
+    var credential = GetCredential();
+    task.JiraUsername = credential.username;
+    task.JiraPassword = credential.password;
+  }
+
+  private void ReleaseVersion (string currentVersionID, string nextVersionID, bool squashUnreleased)
+  {
+    if (squashUnreleased)
+    {
+      var releaseVersion = new JiraReleaseVersionAndSquashUnreleased
+                           {
+                               JiraUrl = JiraUrlWithPostfix(),
+                               VersionID = currentVersionID,
+                               NextVersionID = nextVersionID,
+                               ProjectKey = _config.Jira.JiraProjectKey
+                           };
+      AddAuthentication(releaseVersion);
+      releaseVersion.Execute();
+    }
+    else
+    {
+      var releaseVersion = new JiraReleaseVersion
+                           {
+                               JiraUrl = JiraUrlWithPostfix(),
+                               VersionID = currentVersionID,
+                               NextVersionID = nextVersionID
+                           };
+      AddAuthentication(releaseVersion);
+      releaseVersion.Execute();
+    }
+  }
+
+  private (string username, string password) GetCredential ()
+  {
+    throw new System.NotImplementedException();
+  }
+}
