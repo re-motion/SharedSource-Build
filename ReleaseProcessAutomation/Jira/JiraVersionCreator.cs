5eb0851fba4f724242c517ffe716c0a9f00e7829
Lukas Hartinger <lukas.hartinger@rubicon.eu>
2022-06-07 08:33:12 +0200

    RMSRCBUILD-268 Removed dead code

--- ReleaseProcessAutomation/Jira/JiraVersionCreator.cs
+++ ReleaseProcessAutomation/Jira/JiraVersionCreator.cs
@@ -34,34 +34,6 @@ public class JiraVersionCreator : JiraTask
       base(jiraUsername, jiraPassword)
   {
   }
-
-  public void CreateNewVersion (string jiraUrl, string jiraProject, string versionPattern, bool sortVersion, int versionComponentToIncrement, DayOfWeek versionReleaseWeekday)
-  {
-    if (string.IsNullOrEmpty(jiraUrl))
-    {
-      throw new InvalidOperationException("Jira url was not assigned.");
-    }
-
-    if (string.IsNullOrEmpty(jiraProject))
-    {
-      throw new InvalidOperationException("Jira project was not assigned.");
-    }
-      
-    if (string.IsNullOrEmpty(versionPattern))
-    {
-      throw new InvalidOperationException("Version pattern was not assigned.");
-    }
-      
-    JiraRestClient restClient = new JiraRestClient (jiraUrl, Authenticator);
-    IJiraProjectVersionService service = new JiraProjectVersionService (restClient);
-    IJiraProjectVersionFinder finder = new JiraProjectVersionFinder (restClient);
-    var jiraProjectVersionRepairer = new JiraProjectVersionRepairer (service, finder);
-
-    var createdVersionId = service.CreateSubsequentVersion (jiraProject, versionPattern, versionComponentToIncrement, versionReleaseWeekday);
-
-    if (sortVersion)
-      jiraProjectVersionRepairer.RepairVersionPosition (createdVersionId);
-  }
   
   public string CreateNewVersionWithVersionNumber (string jiraUrl, string jiraProject, string versionNumber )
   {
