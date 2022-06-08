0497da987a8613b54a6185781a6df9eef0bb03fd
Lukas Hartinger <lukas.hartinger@rubicon.eu>
2022-06-08 11:18:49 +0200

    RMSRCBUILD-268 Added JiraRestClientProvider

--- ReleaseProcessAutomation/Jira/Jira.cs
+++ ReleaseProcessAutomation/Jira/Jira.cs
@@ -2,6 +2,7 @@ using System;
 using ReleaseProcessAutomation.Configuration.Data;
 using ReleaseProcessAutomation.Jira.CredentialManagement;
 using ReleaseProcessAutomation.Jira.ServiceFacadeImplementations;
+using ReleaseProcessAutomation.Jira.Utility;
 using RestSharp.Authenticators;
 
 namespace ReleaseProcessAutomation.Jira;
@@ -11,44 +12,24 @@ public class Jira
 {
     private readonly Config _config;
     private readonly IJiraCredentialManager _jiraCredentialManager;
+    private readonly IJiraRestClientProvider _jiraRestClientProvider;
 
     private JiraVersionCreator? _versionCreator;
 
-    public IJiraVersionCreator VersionCreator => _versionCreator ??= new JiraVersionCreator(JiraRestClient);
+    public IJiraVersionCreator VersionCreator => _versionCreator ??= new JiraVersionCreator(_jiraRestClientProvider.GetJiraRestClient());
 
     private JiraVersionReleaser? _versionReleaser;
-    public IJiraVersionReleaser VersionReleaser => _versionReleaser ??= new JiraVersionReleaser(JiraRestClient);
+    public IJiraVersionReleaser VersionReleaser => _versionReleaser ??= new JiraVersionReleaser(_jiraRestClientProvider.GetJiraRestClient());
     
     public IJiraAuthenticationWrapper AuthenticationWrapper { get; }
-
-    private JiraRestClient? _jiraRestClient;
-    private JiraRestClient JiraRestClient
-    {
-        get
-        {
-            if (_jiraRestClient == null)
-            {
-                if (_config.Jira.UseNTLM)
-                {
-                    _jiraRestClient = new JiraRestClient(_config.Jira.JiraURL, new NtlmAuthenticator());
-                }
-                else
-                {
-                    var credentials = _jiraCredentialManager.GetCredential(_config.Jira.JiraURL);
-                    //var credentials = new Credentials { Username = "user", Password = "password" };
-                    _jiraRestClient = new JiraRestClient(_config.Jira.JiraURL, new HttpBasicAuthenticator(credentials.Username, credentials.Password));
-                }
-            }
-
-            return _jiraRestClient;
-        }
-    }
+    
 
 
-    public Jira (Config config, IJiraCredentialManager jiraCredentialManager, IJiraAuthenticationWrapper authenticationWrapper)
+    public Jira (Config config, IJiraCredentialManager jiraCredentialManager, IJiraAuthenticationWrapper authenticationWrapper, IJiraRestClientProvider jiraRestClientProvider)
     {
         _config = config;
         _jiraCredentialManager = jiraCredentialManager;
+        _jiraRestClientProvider = jiraRestClientProvider;
         AuthenticationWrapper = authenticationWrapper;
     }
 }
---
+++ ReleaseProcessAutomation/Jira/Utility/JiraRestClientProvider.cs
@@ -0,0 +1,37 @@
+using ReleaseProcessAutomation.Configuration.Data;
+using ReleaseProcessAutomation.Jira.CredentialManagement;
+using ReleaseProcessAutomation.Jira.ServiceFacadeImplementations;
+using RestSharp.Authenticators;
+
+namespace ReleaseProcessAutomation.Jira.Utility;
+
+public class JiraRestClientProvider
+    : IJiraRestClientProvider
+{
+  private JiraRestClient? _jiraRestClient;
+  private readonly Config _config;
+  private readonly IJiraCredentialManager _jiraCredentialManager;
+
+  public JiraRestClient GetJiraRestClient()
+  {
+    if (_jiraRestClient != null)
+      return _jiraRestClient;
+    
+    if (_config.Jira.UseNTLM)
+    {
+      _jiraRestClient = new JiraRestClient(_config.Jira.JiraURL, new NtlmAuthenticator());
+    }
+    else
+    {
+      var credentials = _jiraCredentialManager.GetCredential(_config.Jira.JiraURL);
+      _jiraRestClient = new JiraRestClient(_config.Jira.JiraURL, new HttpBasicAuthenticator(credentials.Username, credentials.Password));
+    }
+    return _jiraRestClient;
+}
+
+  public JiraRestClientProvider (Config config, IJiraCredentialManager jiraCredentialManager)
+  {
+    _jiraCredentialManager = jiraCredentialManager;
+    _config = config;
+  }
+}
